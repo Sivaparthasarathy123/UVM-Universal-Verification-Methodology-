1079c53f45fd970b9e690b2df02469f26c327062
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-02-07 07:51:08 +0530

    Adding synchronous FIFO parameter package

---
+++ Synchronous FIFO/12) Package.sv
@@ -0,0 +1,10 @@
+// SYNCHRONOUS FIFO - UVM PARAMETER PACKAGE
+`ifndef SYNC_FIFO_PKG_SV
+`define SYNC_FIFO_PKG_SV
+
+package sync_fifo_pkg;
+  parameter DEPTH = 16;
+  parameter WIDTH = 16;
+endpackage
+
+`endif
