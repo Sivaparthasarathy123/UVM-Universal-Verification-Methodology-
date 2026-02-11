6c3c412de45d01e365b6e6a81ff8019c05b19d05
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-02-12 00:00:26 +0530

    Add UVM parameter package for asynchronous FIFO

---
+++ Asynchronous FIFO/09) Package.sv
@@ -0,0 +1,11 @@
+// ASYNCHRONOUS FIFO - UVM PARAMETER PACKAGE
+
+`ifndef ASYNC_FIFO_PKG_SV
+`define ASYNC_FIFO_PKG_SV
+
+package async_fifo_pkg;
+  parameter DEPTH = 8;
+  parameter WIDTH = 8;
+endpackage
+
+`endif
