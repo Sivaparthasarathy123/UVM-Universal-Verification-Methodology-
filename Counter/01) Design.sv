6e3633c3e5a3e6aa39fe652495ad323b374bb5e4
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-03-10 23:50:56 +0530

    Design

---
+++ Counter/01) Design.sv
@@ -0,0 +1,33 @@
+// UVM UP DOWN COUNTER
+module nbit_updown_modn_counter #(
+  parameter N = 4  
+)(
+  input clk,       
+  input rst,       
+  input up_down,   
+  output reg [N-1:0] count
+);
+
+  localparam MAX = (2**N) - 1; 
+
+  always @(posedge clk) begin
+    if (rst) begin
+      count <= 0;
+    end
+    else begin
+      if (up_down) begin
+        if (count == MAX)
+          count <= 0;    
+        else
+          count <= count + 1;
+      end
+      else begin
+        if (count == 0)
+          count <= MAX;  
+        else
+          count <= count - 1;
+      end
+    end
+  end
+
+endmodule
