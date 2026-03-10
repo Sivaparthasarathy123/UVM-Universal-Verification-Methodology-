a6be4ca2bec25b927f3f64990c6ef26aeba3d2d9
Sivaparthasarathy G M <161225307+Sivaparthasarathy123@users.noreply.github.com>
2026-03-10 23:59:21 +0530

    Monitor

---
+++ Counter/07) Monitor.sv
@@ -0,0 +1,41 @@
+// UP DOWN COUNTER - UVM MONITOR
+`ifndef COUNTER_MONITOR
+`define COUNTER_MONITOR
+
+class counter_monitor extends uvm_monitor;
+  `uvm_component_utils(counter_monitor)
+
+  virtual counter_intf#(N) vintf;
+
+  // Analysis port with parameter
+  uvm_analysis_port #(counter_seq_item) analysis_port;
+
+  function new(string name = "counter_monitor", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if(!uvm_config_db #(virtual counter_intf#(N))::get(this,"","vintf",vintf))
+      `uvm_fatal("NO_VINTF","Virtual Interface not found in monitor")
+
+      analysis_port = new("analysis_port", this);
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    forever begin
+      counter_seq_item tr;
+      @(vintf.mon); 
+
+      tr = counter_seq_item::type_id::create("tr");
+
+      tr.rst      = vintf.mon.rst;
+      tr.up_down  = vintf.mon.up_down;
+      tr.count    = vintf.mon.count;
+
+      analysis_port.write(tr);
+    end
+  endtask
+endclass
+
+`endif
